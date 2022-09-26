cea39701afae09d3e2e7bb8d7d60b292c9cd8965
yukiya <hosiyky33077@gmail.com>
2022-09-26 14:30:35 +0900

    61

--- WebToDoStarter/src/main/java/com/example/demo/service/TaskServiceImpl.java
+++ WebToDoStarter/src/main/java/com/example/demo/service/TaskServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dao.TaskDao;
@@ -25,11 +26,12 @@ public class TaskServiceImpl implements TaskService {
 	@Override
 	public Optional<Task> getTask(int id) {
 
-		//削除してください
-		Optional<Task> taskOpt = null;
-		return taskOpt;
-
-		//Optional<Task>一件を取得 idが無ければ例外発生　
+		//Optional<Task>一件を取得 idが無ければ例外発生
+		try {
+			return dao.findById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new TaskNotFoundException("指定されたタスクが存在しません");
+		}
 
 	}
 
@@ -42,6 +44,9 @@ public class TaskServiceImpl implements TaskService {
 	public void update(Task task) {
 
 		//Taskを更新　idが無ければ例外発生
+		if(dao.update(task) == 0) {
+			throw new TaskNotFoundException("更新するタスクが存在しません");
+		}
 
 	}
 
@@ -49,6 +54,9 @@ public class TaskServiceImpl implements TaskService {
 	public void deleteById(int id) {
 
 		//Taskを更新 idがなければ例外発生
+		if(dao.deleteById(id) == 0) {
+			throw new TaskNotFoundException("削除するタスクが存在しません");
+		}
 
 	}
 
